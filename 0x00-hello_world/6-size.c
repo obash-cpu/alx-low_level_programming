8796b525b133f21a7f643e77deb19c836fd4cfe6
obash-cpu <absalomobala@gmail.com>
2022-10-16 06:57:26 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,12 @@
 */
 int main(void)
 {
-	int a;
-	long b;
-	long long int c;
-	char r;
-	float f;
 
-	print("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(r));
-	print("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	print("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	print("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	print("Size of a char: %lu byte(s)\n", sizeof(char));
+	print("Size of an int: %lu byte(s)\n", sizeof(int));
+	print("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	print("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	print("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
 
