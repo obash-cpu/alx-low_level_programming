e91dba2806c4cb8ce1c0aa204be81f5e2908ab65
obash-cpu <absalomobala@gmail.com>
2022-10-19 16:49:56 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,40 +1,24 @@
 #include "main.h"
 /**
  * jack_bauer - print every minute of the day of jack Bauer
- * Description: start from 00:00 to 23:59
+ * Return: returns 0
 */
 void jack_bauer(void)
 {
-	int x = 0;
-	int a = 0, b = 0, c = 0, d = 0;
-
-	while (x < 1440)
-	{
-		_putchar(a + '0');
-		_putchar(b + '0');
-		_putchar(':');
-		_putchar(c + '0');
-		_putchar(d + '0');
-		_putchar('\n');
-
-		d++;
-		if (d > 9)
+	int hr;
+	int min;
+		for (hr = 0; hr < 24; hr++)
 		{
-			d = 0;
-			c++;
+			for (min = 0; min < 60; min++)
+			{
+				_putchar((hr / 10) + '0');
+				_putchar((hr % 10) + '0');
+				_putchar(':');
+				_putchar((min / 10) + '0'0);
+				_putchar((min % 10) + '0');
+				_putchar('\n');
+			}
 		}
-		if (c > 5)
-		{
-			c = 0;
-			b++;
-		}
-		if (b > 9)
-		{
-			b = 0;
-			a++;
-		}
-		x++;
-	}
 }
 
 
