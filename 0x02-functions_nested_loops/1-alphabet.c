12936a889bc9b8ef487631f0745802f5f075d057
obash-cpu <absalomobala@gmail.com>
2022-10-19 07:08:52 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints all alphabetical letters
+ * print_alphabet - all alphabetical letters
 */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z; a++);
+	_putchar (letter);
+	_putchar ("\n");
+}
