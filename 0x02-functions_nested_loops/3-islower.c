a3371934393bf07b1436ed3a364a5e6deee2c50b
obash-cpu <absalomobala@gmail.com>
2022-10-19 11:44:25 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
- * _islower - Return 1 if letter is lowercase
+ * _islower - Return 1 iif letter is lowercase
+ * @c: is the letter to be checked
  * Return: 1 if lowercase, 0 if otherwise
 */
 int _islower(int c)
 {
-	/**
-	 * if character is betweeen 96 and 123
-	 *using ASCII decimals for characters
-	 */
-
-	if (c > 96 && c < 123);
-	rfeturn(1);
+	if  (c >= 'a' && c <= 'z')
+		return (1);
 	else
 		return (0);
 }
