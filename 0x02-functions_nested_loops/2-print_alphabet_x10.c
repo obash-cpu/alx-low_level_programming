741500f1ec2ee13878daad5e5d0a61019d3e6526
obash-cpu <absalomobala@gmail.com>
2022-10-19 10:55:06 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,22 @@
 #include "main.h"
 /**
- * main - prints 
+ * print_alphabet_x10 - prints alphabet 10 in lowercase
+ * Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
