e0cf9dea6c191622814f143b745e80844c456771
obash-cpu <absalomobala@gmail.com>
2022-10-19 12:45:03 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isalpha - checks for alphabetical letters
- * @c:is the letter to be checked
- *Return: 1 if c is alphabet and 0 if not alphabet
+ * _isalpha - checks for char alphabet
+ * @c:the char to be checked
+ *Return: 1 if char is letter lower or upper, 0 otherwise
 */
 int _isalpha(int c)
 
