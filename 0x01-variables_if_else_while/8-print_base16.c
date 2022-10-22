f783d16e94570818ede451d2626b8fd03a774524
obash-cpu <absalomobala@gmail.com>
2022-10-22 02:50:10 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: aLWAYS 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n);
+	}
+	for (char = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
