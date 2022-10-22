0a6a82f0f8e16582f7ebb2ee086461b0cca17a75
obash-cpu <absalomobala@gmail.com>
2022-10-22 02:39:14 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in reversed using putchar
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
