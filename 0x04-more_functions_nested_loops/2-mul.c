14c4061be7a5cd51ae77b27db516e725cda9d435
obash-cpu <absalomobala@gmail.com>
2022-10-23 07:05:47 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first value
+ * @b: second value
+ * Return: 0 when well executed
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
