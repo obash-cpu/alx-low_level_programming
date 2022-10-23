aeb1de87655d88551c794eb3ad947ffd77005374
obash-cpu <absalomobala@gmail.com>
2022-10-23 11:54:43 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,21 +6,17 @@
  * followed by a new line.
  * Return: void
  */
+
 void print_most_numbers(void)
 {
-	int i;
+	char c;
 
-
-	i = 0;
-	while (i < 10)
+	for (c = '0'; c <= '9'; c++)
 
 	{
-		if (i != 2 && i != 4)
-
-			_putchar(i + '0');
-		i++;
-
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
-
 	_putchar('\n');
+
 }
