c2340805b61c1d3e0f9f141323c42004fc644182
obash-cpu <absalomobala@gmail.com>
2022-10-23 07:37:41 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * print_numbers - prints numbers from 0 to 9
- * Return: Always 0
+ * Return: void
  */
 void print_numbers(void)
 {
-	char c;
+	int c;
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
