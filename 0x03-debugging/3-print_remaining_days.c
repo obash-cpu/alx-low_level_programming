d742c9234fa72183bfb43d747f835633ad9f36d0
obash-cpu <absalomobala@gmail.com>
2022-10-23 02:19:34 -0700

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,8 +29,8 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-	printf("Invalid date: %d\n", day);
-	printf("Remaining days: %\n", 365 - day);
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 365 - day);
 	}
 	}
 }
