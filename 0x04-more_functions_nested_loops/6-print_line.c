7cde051b76bbe3b49179ed1563f746921ce8b395
obash-cpu <absalomobala@gmail.com>
2022-10-24 04:24:42 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * prin_line - a funcltion that draws a straight line in the terminal.
+ * followed by a nwe line.
+ * @n: An input integer
+ * Return: Alwaysm0
+ */
+
 void print_line(int n)
 {
 	int i = 0;
