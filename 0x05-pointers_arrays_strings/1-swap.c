91c5db891e0c41da967a5a4ad55fe1602046524a
obash-cpu <absalomobala@gmail.com>
2022-10-25 01:15:16 -0700

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * swap_int: - swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * @a: pointer 1
  * @b: pointer 2
  * Return: void
  */
 void swap_int(int *a, int *b)
 {
-	int x = *a;
+	int c;
+
+	int c = *a;
 	*a = *b;
-	*b = x;
+	*b = c;
 }
 
