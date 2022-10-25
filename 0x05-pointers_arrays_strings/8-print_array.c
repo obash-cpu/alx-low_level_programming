92ee07b5f4427f36b089e95381cd03698aad9950
obash-cpu <absalomobala@gmail.com>
2022-10-25 09:52:51 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#inckude "main.h"
+
+/**
+ * print_array - prints n elements of an array if integers
+ * @a: input array
+ * @n: input n elements
+ * Return: no return
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
