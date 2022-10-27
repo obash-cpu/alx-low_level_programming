95f56dce2445c7de157ad51e773b4cb40da9b6e5
obash-cpu <absalomobala@gmail.com>
2022-10-27 09:01:36 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,12 +4,12 @@
  * _strcat - a function that concatenates two strings
  * @src: is the source string to be appended
  * @dest: the string to append
- * Return: a pointer to the resulting string
+ * Return: Always 0
  */
 char *_strcat(char *dest, char *src)
 {
-	char destination[] = "my c string ";
-	char source[] = "practice made clear";
+	char destination[30] = "my c string ";
+	char source[30] = "practice made clear";
 
 	strcat(destination, source);
 	printf("Concatenated String: %s\n", destination);
