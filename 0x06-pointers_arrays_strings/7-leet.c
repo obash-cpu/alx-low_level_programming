2e8f8e75c63a02fe8b093c60a3395be6fb617ebb
obash-cpu <absalomobala@gmail.com>
2022-10-29 10:04:51 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,4 +7,20 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, x;
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replace[] = {'4'', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if (s[i] == find[x])
+			{
+				s[i] = replace[x / 2];
+				x = 9;
+			}
+		}
+	}
+	return (s);
+}
