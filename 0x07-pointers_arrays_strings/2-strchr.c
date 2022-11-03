f648c8b2c70bb698cb1a95d4a1d13b41d4eaa775
obash-cpu <absalomobala@gmail.com>
2022-11-03 07:00:26 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - returns the occurrence of character c
+ * @c: is the character to look for
+ * @s: is the string where to look for c
+ * Return: a pointer to the string s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	if (i == c)
+	{
+		return (c);
+	}
+	c++;
+	else
+	{
+		return (\0);
+	}
+}
+
+
