7d5a5a2577bf3f94749bd1f96a1280db030a947a
obash-cpu <absalomobala@gmail.com>
2022-11-15 06:44:45 -0800

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,9 @@ struct dog
 
 /**
  * dog_t - typedef for struct dog
+ *
  */
+typedef struct dog dog_d;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
