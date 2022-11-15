a451d3b40d1a370e87cc0cfb2be70d8885ed9a3b
obash-cpu <absalomobala@gmail.com>
2022-11-15 06:01:51 -0800

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,8 +15,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedeffor struct dog
+ */
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
