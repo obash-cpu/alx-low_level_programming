77bf962bcdbd4329fe3b652f2e8f11bd180f2024
obash-cpu <absalomobala@gmail.com>
2022-11-25 23:29:28 -0800

    task4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,30 +9,33 @@
  */
 int main(int argc, char *argv[])
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+		if (index == bytes - 1)
 
-	char *opc = (char *) main;
+			continue;
 
-	nbytes = atoi(argv[1]);
-			if (nbytes < 0)
-			{
-				printf("Error\n");
-				exit(2);
-			}
-			for (i = 0; i < nbytes; i++)
-			{
-				printf("%02x", opc[i] & 0xFF);
-				if (i != nbytes - 1)
-					printf(" ");
-			}
+		printf(" ");
+		address++;
 
-			printf("\n");
-			return (0);
+	}
+	printf("\n");
+	return (0);
 }
