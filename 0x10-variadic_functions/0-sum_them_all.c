ea2cb8d6ee7fbf5074e6b03c501cd5fa887289f2
obash-cpu <absalomobala@gmail.com>
2022-11-26 00:58:14 -0800

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
 
-	va_start(nums, n);
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
-	va_end(nums);
+	if (n == 0)
+	return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+
+	sum += va_arg(valist, int);
+
+	va_end(valist);
+
 	return (sum);
 }
