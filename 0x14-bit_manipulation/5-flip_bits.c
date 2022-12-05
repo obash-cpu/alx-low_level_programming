52c0cb3d7ee0f2c9b3478dda3bab2bea4a53382a
obash-cpu <absalomobala@gmail.com>
2022-12-05 06:26:06 -0800

    task5 correction

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,13 +8,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m, bits = 0;
+	unsigned int flip = 0, comp, comp1;
 
-	while (xor > 0)
+	while (!(n == 0 && m == 0))
 	{
-		while (xor > 0)
-			xor >>= 1;
+		comp = n & 1;
+		comp1 = m & 1;
+		n = n >> 1;
+		m = m >> 1;
+		if (comp != comp1)
+			flip += 1;
 	}
-	return (bits);
-
+	return (flip);
 }
