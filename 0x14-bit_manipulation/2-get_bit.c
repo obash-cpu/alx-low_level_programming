5523d8e7a36406d4d0fca208ed4283188478e1fd
obash-cpu <absalomobala@gmail.com>
2022-12-05 06:30:54 -0800

    task2 correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index > 63)
 		return (-1);
-	if ((n & (1 << index)) == 0)
-		return (0)
-			if ((n & (1 << index)) == 0)
-			return (1);
-	return (1);
+	if (index == 0)
+	{
+		return (n & 1);
+	}
+	return (get_bit(n >> 1, index - 1));
 
 }
