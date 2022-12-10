741c2b6eec542045f6cc7ec52dd633158e616757
obash-cpu <absalomobala@gmail.com>
2022-12-10 00:29:09 -0800

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,37 +7,33 @@
  * Retutrn: void
 */
 void times_table(void)
+
 {
-	int row, column, product, tens, ones;
+	int rone, cone, d;
 
-	for (row = 0; row <= 9; row++)
+	for (rone = 0; rone <= 9; rone++)
 	{
-		for (column = 0; column <= 9; column++)
-
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
 		{
-			product = row * column;
-			tens = product / 10;
-			ones = product % 10;
-
-			if (column == 0)
+			d = (rone * cone);
+			if ((d / 10) > 0)
 			{
-				_putchar('0');
-			}
-			else if (product < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(ones + '0');
+				_putchar((d / 10) + '0');
 			}
 			else
 			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
 			}
 		}
 		_putchar('\n');
 	}
-i}
