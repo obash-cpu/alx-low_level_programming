4c2b2cca437c482ebf8f6ad82ebd012f8337cb13
obash-cpu <absalomobala@gmail.com>
2022-12-14 04:54:20 -0800

    task correction

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,11 +7,7 @@
  * @symbol: A symbol representing a data type
  * @print: A function pointer to a function that prints
  */
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list arg);
-} printer_t;
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
