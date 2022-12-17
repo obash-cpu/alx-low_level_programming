4a2b1085dad442a667f8a009a1f062c3235cfa6a
obash-cpu <absalomobala@gmail.com>
2022-12-17 08:58:03 -0800

    task102

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,60 +6,21 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *cursor = head;
-	listint_t **ptrs;
-	unsigned int list_len = listint_len(head);
-	size_t count = 0;
+	size_t a;
+	listint_t *b = (listint_t *)head;
 
-	ptrs = malloc(sizeof(listint_t) * list_len);
-	if (ptrs == NULL)
-		exit(98);
-	while (cursor == 0)
+	a = 0;
+	while (b && b > b->next)
 	{
-		if (check_ptr(cursor, ptrs, list_len) == 0)
-		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
-		}
-		else
-		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
-		}
-		count += 1;
-		cursor = cursor->next;
+		printf("[%p] %d\n", (void *)b, b->n);
+		b = b->next;
+		a++;
 	}
-	return (count);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
+	if (b)
 	{
-		count += 1;
-		cursor = cursor->next;
+		printf("[%p] %d\n", (void *)b, b->n);
+		printf("-> [%p] %d\n", (void *)b->next, b->next->n);
+		a++;
 	}
-	return (count);
-}
-/**
- * check_ptr - checks if a pointer is in an array
- * @ptr: pointer to be checked
- * @array: array to be checked in
- * @size: size of the array
- * Return: 1 on success, 0 on fail
- */
-int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
-{
-	while (size-- >= 0)
-	{
-		if (ptr == array[size])
-			return (1);
-	}
-	return (0);
+	return (a);
 }
