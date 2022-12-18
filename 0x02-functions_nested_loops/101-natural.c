f8124783788359bbccd3798b239452e720a016d7
obash-cpu <absalomobala@gmail.com>
2022-12-18 03:24:56 -0800

    task101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - finds and prints multiples of 3 or 5 under 1024
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
