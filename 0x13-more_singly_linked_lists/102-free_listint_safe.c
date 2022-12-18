99ceee9d396a9554712d6e544f45cab4d7daa1d5
obash-cpu <absalomobala@gmail.com>
2022-12-17 21:45:22 -0800

    task13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,33 +11,26 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *runner, *head;
-	size_t h_i, r_i;
+	size_t nodeCount = 0;
+	listint_t *temp = NULL;
 
-	if (h == NULL || *h == NULL)
-		return (0);
-
-	current = *h;
-	head = *h;
-	h_i = 0;
-
-	while (head != NULL)
+	if (!(h && *h))
+		return (nodeCount);
+	while (*h)
 	{
-		runner = *h;
-		for (r_i = 0; r_i < h_i; r_i++)
+		nodeCount++;
+		if (*h > (*h)->next)
 		{
-			if (runner == current)
-			{
-				*h = NULL;
-			return (h_i);
-			}
-			runner = runner->next;
+			temp = *h;
+			*h = (*h)->next;
+			free(temp);
 		}
-		current = head->next;
-		free(head);
-		head = current;
-		h_i++;
+		else
+		{
+			free(*h);
+			*h = NULL;
+		}
 	}
 	*h = NULL;
-	return (h_i);
+	return (nodeCount);
 }
