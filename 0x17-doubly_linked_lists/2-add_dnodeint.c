b0320a45b29fc34d40a8b3d7a19aa9118c664c91
obash-cpu <absalomobala@gmail.com>
2023-01-12 02:12:09 -0800

    task

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,17 +9,22 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-
-	new = malloc(sizeof(dlistint_t));
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
-		(return) NULL;
+		return (NULL);
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
-	if (*head != NULL)
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = *head;
 		(*head)->prev = new;
-	*head = new;
+		*head = new;
+	}
 	return (new);
 }
