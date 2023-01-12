0c9c32a9b27a2f3bc08f913ccfdb7c2d087fb05a
obash-cpu <absalomobala@gmail.com>
2023-01-12 02:03:24 -0800

    task

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_dnodeint - add a node at head of d list
+ * @head: the head
+ * @n: the int to add
+ * Return: addy of new node or NULL on fail
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		(return) NULL;
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
+}
