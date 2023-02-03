fc270187308eb44a7ec6dfd39b173ba7eecb4cec
obash-cpu <absalomobala@gmail.com>
2023-02-03 05:02:38 -0800

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,3 @@
-/**
- * File: 0-hash_table_create.c
- * Auth: Gedeon Obae Gekonge
- */
-
 #include "hash_tables.h"
 /**
  * hash_table_create - Creates a hash table.
@@ -11,18 +6,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+	unsigned int i = 0;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
-	ht = malloc(sizeof(hash_table_t));
+
 	if (ht == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-
+	}
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-	for (i = 0; i < size; i++)
+	}
+	for (; i < size; i++)
 		ht->array[i] = NULL;
 
 	return (ht);
