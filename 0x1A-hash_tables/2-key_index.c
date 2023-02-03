7836a812e3a2c81fde1db8d8fcb68f0d39fae74f
obash-cpu <absalomobala@gmail.com>
2023-02-03 05:30:40 -0800

    task

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int result;
+	unsigned long int result = 0;
 
 	result = hash_djb2(key) % size;
 	return (result);
